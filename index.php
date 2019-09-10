b6874f18808961f3d6445d99ddce185f6b2e216c
Lgjoo <44471528+Lgjoo@users.noreply.github.com>
2019-09-09 21:11:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta id="viewport" name="viewport" content="width=device-width, user-scalable=no" />
+		<title>Tributo - Von Neumann</title>
+		<link rel="stylesheet" type="text/css" href="./assets/css/style.css" />
+	</head>
+	<body>
+		<header class="centralizar">
+			<div class="area-util">
+				<div class="centralizar tributo">
+					<div class="tributo__title">
+						John Von Neumann
+					</div>
+					<div class="tributo__subtitle">
+						Matemático
+					</div>
+				</div>
+			</div>
+		</header>
+		<section class="centralizar">
+			<div class="area-util centralizar">
+				<div class="image-area">
+					<img src="./assets/images/JohnvonNeumann.png" />
+					<div class="image-area__foot-note">
+						John Von Neumann
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="centralizar">
+			<div class="area-util centralizar">
+				<div class="resumo">
+					John von Neumann, nascido Margittai Neumann János Lajos (Budapeste, 28 de dezembro de 1903 — Washington, D.C., 8 de fevereiro de 1957) foi um matemático húngaro de origem judaica, naturalizado estadunidense.
+					<br />
+					<br />
+					Contribuiu na teoria dos conjuntos, análise funcional, teoria ergódica, mecânica quântica, ciência da computação, economia, teoria dos jogos, análise numérica, hidrodinâmica das explosões, estatística e muitas outras as áreas da matemática. De fato é considerado um dos mais importantes matemáticos do século XX.[2]
+					<br />
+					<br />
+					Foi membro do Instituto de Estudos Avançados de Princeton, Nova Jérsei, do qual também faziam parte Albert Einstein e Erwin Panofsky, quando emigraram para os Estados Unidos, além de Kurt Gödel, Robert Oppenheimer, George F. Kennan e Hermann Weyl. Com Edward Teller e Stanisław Ulam, von Neumann trabalhou em desenvolvimentos chave da Física Nuclear, relacionados com reações termonucleares e com a bomba de hidrogênio. Participou também do Projeto Manhattan, responsável pelo desenvolvimento das primeiras bombas atômicas.
+					<br />
+					<br />
+					Foi professor na Universidade de Princeton e um dos construtores do ENIAC. Entre os anos de 1946 e 1953, von Neumann integrou o grupo reunido sob o nome de Macy Conferences, contribuindo para a consolidação da teoria cibernética junto com outros cientistas renomados: Gregory Bateson, Heinz von Foerster, Kurt Lewin, Margaret Mead, Norbert Wiener, Paul Lazarsfeld, William Ross Ashby, Claude Shannon, Erik Erikson e Max Delbrück, entre outros. Von Neumann faleceu pouco depois, aos 53 anos, vítima de um tumor cerebral. 
+				</div>
+			</div>
+		</section>
+		<section class="centralizar">
+			<div class="area-util centralizar">
+				<div class="timeline">
+					<div class="timeline__detalhes esquerda">
+						<div class="timeline__item">
+							<div class="data">
+								28/12/1903
+							</div>
+							<div class="texto">
+								Nascimento de Von Neumann
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								18/04/1925
+							</div>
+							<div class="texto">
+								Diplomado em Engenharia Química
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								18/07/1925
+							</div>
+							<div class="texto">
+								Uma axiomatização da teoria dos conjuntos
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								18/04/1926
+							</div>
+							<div class="texto">
+								Doutorado em Matemática
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								18/04/1928
+							</div>
+							<div class="texto">
+								Teoria dos jogos de salão
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								27/10/1932
+							</div>
+							<div class="texto">
+								Sobre certas equações da economia e uma generalização do teorema de ponto fixo de Brouwer
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								20/07/1933
+							</div>
+							<div class="texto">
+								Professor na IAS(Instituto para Estudos Avançados)
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								19/03/1940
+							</div>
+							<div class="texto">
+								Álgebras de Von Neumann
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								29/09/1943
+							</div>
+							<div class="texto">
+								Projeto Manhattan(2ª Guerra Mundial)
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								27/01/1944
+							</div>
+							<div class="texto">
+								Contribuições ao ENIAC
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								27/01/1946
+							</div>
+							<div class="texto">
+								Melhoria dos métodos e meios de utilização da energia nuclear
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								15/01/1950
+							</div>
+							<div class="texto">
+								Consultor da WSEG(Grupo de Avaliação de Sistemas de Armas)
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								27/01/1954
+							</div>
+							<div class="texto">
+								Arquiteto da comissão de energia atômica
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								28/07/1956
+							</div>
+							<div class="texto">
+								Medalha da liberdade e prêmios
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								08/02/1957
+							</div>
+							<div class="texto">
+								Morte de Von Neumann
+							</div>
+						</div>
+					</div>
+					<div class="timeline__linha"></div>
+					<div class="timeline__detalhes direita">
+						<div class="timeline__item">
+							<div class="data">
+								28/12/1903
+							</div>
+							<div class="texto">
+								Nascimento de Von Neumann
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								18/04/1925
+							</div>
+							<div class="texto">
+								Diplomado em Engenharia Química
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								18/07/1925
+							</div>
+							<div class="texto">
+								Uma axiomatização da teoria dos conjuntos
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								18/04/1926
+							</div>
+							<div class="texto">
+								Doutorado em Matemática
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								18/04/1928
+							</div>
+							<div class="texto">
+								Teoria dos jogos de salão
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								27/10/1932
+							</div>
+							<div class="texto">
+								Sobre certas equações da economia e uma generalização do teorema de ponto fixo de Brouwer
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								20/07/1933
+							</div>
+							<div class="texto">
+								Professor na IAS(Instituto para Estudos Avançados)
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								19/03/1940
+							</div>
+							<div class="texto">
+								Álgebras de Von Neumann
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								29/09/1943
+							</div>
+							<div class="texto">
+								Projeto Manhattan(2ª Guerra Mundial)
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								27/01/1944
+							</div>
+							<div class="texto">
+								Contribuições ao ENIAC
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								27/01/1946
+							</div>
+							<div class="texto">
+								Melhoria dos métodos e meios de utilização da energia nuclear
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								15/01/1950
+							</div>
+							<div class="texto">
+								Consultor da WSEG(Grupo de Avaliação de Sistemas de Armas)
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								27/01/1954
+							</div>
+							<div class="texto">
+								Arquiteto da comissão de energia atômica
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								28/07/1956
+							</div>
+							<div class="texto">
+								Medalha da liberdade e prêmios
+							</div>
+						</div>
+						<div class="timeline__item">
+							<div class="data">
+								08/02/1957
+							</div>
+							<div class="texto">
+								Morte de Von Neumann
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="centralizar">
+			<div class="area-util">
+				<div class="more">
+					Para mais informações, acesse: <a href="https://pt.wikipedia.org/wiki/John_von_Neumann">Wikipedia</a>
+				</div>
+			</div>
+		</section>
+		<footer class="centralizar">
+			<div class="area-util centralizar">
+				<div class="info centralizar">
+					<div class="info__links">
+						<a class="centralizar" href="https://www.linkedin.com/in/luiz-guilherme-silva-dev/">
+							<img src="./assets/images/linkedin.png" />
+						</a>
+						<a class="centralizar" href="https://github.com/Lgjoo">
+							<img src="./assets/images/github.png" />
+						</a>
+					</div>
+					<div class="info__creditos">
+						Criado por Luiz Guilherme Silva, ícones por <a href="https://www.flaticon.com/authors/epiccoders" title="EpicCoders">EpicCoders</a> e <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
